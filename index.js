31641bf2e1aece2d9a22ba81f2d76b929b007515
Matt Brown <matthewtbrown124@gmail.com>
2021-03-24 12:10:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Matt",
+    streetAddress: "111 Drury Lane"
+};
+function updateEmployeeWithKeyAndValue(employee, name, value){
+    const newEmployee={...employee};
+    newEmployee[name] =value;
+    return newEmployee;
+}
+const newEmployee = updateEmployeeWithKeyAndValue(employee,"name", "Tim");
+
+newEmployee.name;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,streetAddress,value){
+    employee[streetAddress]= "12 Broadway"
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,name, value){
+    const newEmployee2={...employee};
+    newEmployee2[name] = value;
+    return newEmployee2;
+}
+const newEmployee2 =deleteFromEmployeeByKey("name", "Tim");
+
+newEmployee2.name;
+
+function destructivelyDeleteFromEmployeeByKey(employee,name,value){
+    employee[name] =value;
+    return employee;
+} 
